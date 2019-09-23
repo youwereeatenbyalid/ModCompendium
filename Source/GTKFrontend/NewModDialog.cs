dc77064f17c73157df7204174b0bceae8a9fc657
youwereeatenbyalid <youwereeatenbyalid@github.com>
2019-09-23 01:56:18 -0400

    added new mod dialog

--- Source/GTKFrontend/NewModDialog.cs
+++ Source/GTKFrontend/NewModDialog.cs
@@ -3,9 +3,27 @@ namespace GTKFrontend
 {
     public partial class NewModDialog : Gtk.Dialog
     {
+        public string ModTitle => titleentry.Text;
+
+        public string Description => descriptionentry.Text;
+
+        public string Version => versionentry.Text;
+
+        public string Author => authorentry.Text;
+
+        public string Url => urlentry.Text;
+        public bool creation = false;
+        public string UpdateUrl => updateurlentry.Text;
         public NewModDialog()
         {
             this.Build();
+            buttonOk.Sensitive = false;
+        }
+
+
+        protected void ontitletextchange(object sender, EventArgs e)
+        {
+            buttonOk.Sensitive = titleentry.Text.Length != 0;
         }
     }
 }
